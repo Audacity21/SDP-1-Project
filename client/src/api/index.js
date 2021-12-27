b6e60464e2a0dea33ea60fe97bc2a16b8b195946
Ankit Seth <iam4nkit@gmail.com>
2021-12-27 21:45:28 +0530

    Delete client directory

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const url = 'https://klez-sap-database.herokuapp.com/posts';
-
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
